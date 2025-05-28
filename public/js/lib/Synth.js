2af329dafdd94d05a33bf60a27815f1401bfe94d
beefoo <brian@youaremyjoy.org>
2025-05-28 15:55:52 -0400

    Implement synth settings

--- public/js/lib/Synth.js
+++ public/js/lib/Synth.js
@@ -1,10 +1,13 @@
 import MathHelper from './MathHelper.js';
+import StringHelper from './StringHelper.js';
+import Throttler from './Throttler.js';
 import * as Tone from '../vendor/Tone.js';
 
 export default class Synth {
   constructor(options = {}) {
     const defaults = {
       debug: false,
+      localStorageKey: 'synth',
     };
     this.options = Object.assign(defaults, options);
     this.init();
@@ -13,15 +16,53 @@ export default class Synth {
   init() {
     this.loaded = false;
     this.lastScheduled = -1;
+    this.loadFromStorage();
+    this.loadListeners();
+  }
+
+  getInputProperties() {
+    const props = {
+      effects: {
+        reverb: {
+          decay: 2,
+        },
+      },
+      synth: {
+        envelope: {},
+        oscillator: {
+          modulationType: 'square',
+        },
+        modulation: {
+          type: 'square',
+        },
+      },
+    };
+    // set reverb
+    props.effects.reverb.wet = parseFloat(
+      document.getElementById('effects-reverb-wet').value,
+    );
+    // set oscillator type
+    props.synth.oscillator.type = document.querySelector(
+      '.oscillator-type:checked',
+    ).value;
+    // set envelope
+    ['attack', 'decay', 'sustain', 'release'].forEach((prop) => {
+      const value = parseFloat(
+        document.getElementById(`synth-envelope-${prop}`).value,
+      );
+      props.synth.envelope[prop] = value;
+    });
+    return props;
   }
 
   load() {
     if (this.loaded) return;
+    const props = this.getInputProperties();
     const effects = {};
     // add distortion
     // effects.distortion = new Tone.Distortion({ distortion: 0.5, wet: 0.5 });
     // add reverb
-    effects.reverb = new Tone.Reverb({ decay: 2, wet: 0.667 });
+    effects.reverb = new Tone.Reverb(props.reverb);
     // attenuate high notes
     effects.lowpass = new Tone.Filter({ frequency: 'C6', type: 'lowpass' });
     // boost low notes
@@ -42,24 +83,58 @@ export default class Synth {
       effects.limiter,
       effects.gain,
     ];
-    this.synth = new Tone.PolySynth(Tone.AMSynth, {
-      envelope: {
-        attack: 0.02, // 0 - 2, default: 0.01
-        decay: 0.2, // 0 - 2, default: 0.1
-        sustain: 0.25, // 0 - 1, default: 1
-        release: 1, // 0 - 5, default: 0.5
-      },
-      oscillator: {
-        type: 'amsquare',
-        modulationType: 'square',
-      },
-      modulation: {
-        type: 'square',
-      },
-    }).chain(...effectChain);
+    this.synth = new Tone.PolySynth(Tone.AMSynth, props.synth).chain(
+      ...effectChain,
+    );
     this.effects = effects;
     Tone.start();
     this.loaded = true;
+  }
+
+  loadFromStorage() {
+    const props = StringHelper.loadStorageData(
+      this.options.localStorageKey,
+      false,
+    );
+
+    if (!props) return;
+
+    // set oscillator type
+    document.getElementById(
+      `synth-oscillator-type-${props.synth.oscillator.type}`,
+    ).checked = true;
+
+    // set envelope values in UI
+    ['attack', 'decay', 'sustain', 'release'].forEach((prop) => {
+      const value = props.synth.envelope[prop];
+      document.getElementById(`synth-envelope-${prop}`).value = value;
+    });
+
+    // set reverb in UI
+    const reverb = props.effects.reverb.wet;
+    document.getElementById(`effects-reverb-wet`).value = reverb;
+
+    this.update();
+  }
+
+  loadListeners() {
+    const throttled = () => this.update();
+    const throttler = new Throttler({
+      throttled,
+      seconds: 0.3,
+    });
+    const $options = document.querySelectorAll('.synth-option');
+    $options.forEach(($option) => {
+      $option.addEventListener('input', (_event) => {
+        throttler.queue();
+      });
+    });
+    const $types = document.querySelectorAll('.oscillator-type');
+    $types.forEach(($option) => {
+      $option.addEventListener('click', (_event) => {
+        throttler.queue();
+      });
+    });
   }
 
   pause() {
@@ -80,5 +155,33 @@ export default class Synth {
     // console.log(this.synth.activeVoices);
     this.synth.triggerAttackRelease(name, duration, future, velocity);
     this.lastScheduled = future;
+  }
+
+  update() {
+    const props = this.getInputProperties();
+
+    // set envelope values in UI
+    ['attack', 'decay', 'sustain', 'release'].forEach((prop) => {
+      const value = props.synth.envelope[prop];
+      document.getElementById(`synth-envelope-${prop}-value`).innerText = value;
+    });
+
+    // set reverb in UI
+    const reverb = props.effects.reverb.wet;
+    document.getElementById(`effects-reverb-wet-value`).innerText = reverb;
+
+    if (!this.loaded) return;
+
+    // update synth
+    this.synth.set(props.synth);
+    // update reverb
+    this.effects.reverb.set(props.effects.reverb);
+
+    this.updateStorage();
+  }
+
+  updateStorage() {
+    const props = this.getInputProperties();
+    StringHelper.saveStorageData(this.options.localStorageKey, props);
   }
 }
