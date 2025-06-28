e20fc3fc974ed102ef9873d55100b0e14a84f2b2
beefoo <brian@youaremyjoy.org>
2025-06-28 11:15:45 -0400

    Minor edit to credits script

--- credits.js
+++ credits.js
@@ -2,7 +2,7 @@ const config = require('./public/js/config.js');
 const scores = config.default.scores;
 
 scores.forEach((score) => {
-  if (!score.active) return;
+  if (!score.active || score.id === 'upload') return;
   let title = score.title;
   if (score.alt !== '') title = `${score.alt} (${score.title})`;
   console.log(`- [${title}](${score.url}) by ${score.creator}, ${score.date}`);
